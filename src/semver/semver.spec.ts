c0203344941e4168b304c81492d2ed8c7e94b599
dangreen <danon0404@gmail.com>
2019-03-22 21:29:02 +0700

    semver test, jsdocs, fixes

---
+++ src/semver/semver.spec.ts
@@ -0,0 +1,587 @@
+import {
+	ISemver
+} from './types';
+import {
+	semverify,
+	compareSemvers,
+	getRequiredSemverPartsCount,
+	rangedSemverToRegExp
+} from './semver';
+
+describe('Semver', () => {
+
+	describe('semverify', () => {
+
+		it('should semverify strings', () => {
+
+			expect(
+				semverify('10')
+			).toEqual(
+				[10, 0, 0]
+			);
+
+			expect(
+				semverify('11.1')
+			).toEqual(
+				[11, 1, 0]
+			);
+
+			expect(
+				semverify('13.0.1')
+			).toEqual(
+				[13, 0, 1]
+			);
+		});
+
+		it('should semverify array', () => {
+
+			expect(
+				semverify([10])
+			).toEqual(
+				[10, 0, 0]
+			);
+
+			expect(
+				semverify(['11', '1'])
+			).toEqual(
+				[11, 1, 0]
+			);
+
+			expect(
+				semverify([13, '0', '1'])
+			).toEqual(
+				[13, 0, 1]
+			);
+		});
+
+		it('should ignore \'all\' version', () => {
+
+			expect(
+				semverify('all')
+			).toEqual(
+				['all', 0, 0]
+			);
+
+			expect(
+				semverify(['all'])
+			).toEqual(
+				['all', 0, 0]
+			);
+		});
+	});
+
+	describe('compareSemvers', () => {
+
+		it('should handle \'all\' version', () => {
+
+			expect(
+				compareSemvers(
+					[10, 1, 1],
+					['all', 0, 0] as any,
+					{}
+				)
+			).toBe(
+				true
+			);
+		});
+
+		describe('allowHigherVersions: false', () => {
+
+			const options = {
+				ignoreMinor:         false,
+				ignorePatch:         false,
+				allowHigherVersions: false
+			};
+
+			it('should compare all parts', () => {
+
+				expect(
+					compareSemvers(
+						[10, 1, 1],
+						[10, 1, 1],
+						options
+					)
+				).toBe(
+					true
+				);
+
+				expect(
+					compareSemvers(
+						[10, 1, 2],
+						[10, 1, 1],
+						options
+					)
+				).toBe(
+					false
+				);
+
+				expect(
+					compareSemvers(
+						[10, 1, 1],
+						[10, 2, 1],
+						options
+					)
+				).toBe(
+					false
+				);
+			});
+
+			it('should compare ignoring patch', () => {
+
+				const ignorePatchOptions = {
+					...options,
+					ignorePatch: true
+				};
+
+				expect(
+					compareSemvers(
+						[10, 1, 1],
+						[10, 1, 1],
+						ignorePatchOptions
+					)
+				).toBe(
+					true
+				);
+
+				expect(
+					compareSemvers(
+						[10, 1, 0],
+						[10, 1, 1],
+						ignorePatchOptions
+					)
+				).toBe(
+					true
+				);
+
+				expect(
+					compareSemvers(
+						[10, 1, 2],
+						[10, 2, 1],
+						ignorePatchOptions
+					)
+				).toBe(
+					false
+				);
+
+				expect(
+					compareSemvers(
+						[10, 1, 2],
+						[12, 2, 1],
+						ignorePatchOptions
+					)
+				).toBe(
+					false
+				);
+			});
+
+			it('should compare ignoring minor', () => {
+
+				const ignoreMinorOptions = {
+					...options,
+					ignoreMinor: true
+				};
+
+				expect(
+					compareSemvers(
+						[10, 1, 1],
+						[10, 1, 1],
+						ignoreMinorOptions
+					)
+				).toBe(
+					true
+				);
+
+				expect(
+					compareSemvers(
+						[10, 1, 0],
+						[10, 2, 1],
+						ignoreMinorOptions
+					)
+				).toBe(
+					true
+				);
+
+				expect(
+					compareSemvers(
+						[10, 1, 2],
+						[11, 2, 1],
+						ignoreMinorOptions
+					)
+				).toBe(
+					false
+				);
+			});
+		});
+
+		describe('allowHigherVersions: true', () => {
+
+			const options = {
+				ignoreMinor:         false,
+				ignorePatch:         false,
+				allowHigherVersions: true
+			};
+
+			it('should compare all parts', () => {
+
+				expect(
+					compareSemvers(
+						[10, 1, 1],
+						[10, 1, 1],
+						options
+					)
+				).toBe(
+					true
+				);
+
+				expect(
+					compareSemvers(
+						[10, 2, 3],
+						[10, 1, 1],
+						options
+					)
+				).toBe(
+					true
+				);
+
+				expect(
+					compareSemvers(
+						[10, 1, 2],
+						[10, 1, 1],
+						options
+					)
+				).toBe(
+					true
+				);
+
+				expect(
+					compareSemvers(
+						[9, 1, 1],
+						[10, 2, 1],
+						options
+					)
+				).toBe(
+					false
+				);
+
+				expect(
+					compareSemvers(
+						[10, 1, 0],
+						[10, 1, 1],
+						options
+					)
+				).toBe(
+					false
+				);
+			});
+
+			it('should compare ignoring patch', () => {
+
+				const ignorePatchOptions = {
+					...options,
+					ignorePatch: true
+				};
+
+				expect(
+					compareSemvers(
+						[10, 1, 1],
+						[10, 1, 1],
+						ignorePatchOptions
+					)
+				).toBe(
+					true
+				);
+
+				expect(
+					compareSemvers(
+						[10, 0, 2],
+						[10, 2, 1],
+						ignorePatchOptions
+					)
+				).toBe(
+					false
+				);
+
+				expect(
+					compareSemvers(
+						[9, 1, 2],
+						[12, 2, 1],
+						ignorePatchOptions
+					)
+				).toBe(
+					false
+				);
+			});
+
+			it('should compare ignoring minor', () => {
+
+				const ignoreMinorOptions = {
+					...options,
+					ignoreMinor: true
+				};
+
+				expect(
+					compareSemvers(
+						[10, 1, 1],
+						[10, 1, 1],
+						ignoreMinorOptions
+					)
+				).toBe(
+					true
+				);
+
+				expect(
+					compareSemvers(
+						[10, 1, 0],
+						[10, 2, 1],
+						ignoreMinorOptions
+					)
+				).toBe(
+					true
+				);
+
+				expect(
+					compareSemvers(
+						[9, 1, 2],
+						[11, 2, 1],
+						ignoreMinorOptions
+					)
+				).toBe(
+					false
+				);
+			});
+		});
+	});
+
+	describe('getRequiredSemverPartsCount', () => {
+
+		describe('allowZeroSubverions: false', () => {
+
+			const options = {
+				ignoreMinor:         false,
+				ignorePatch:         false,
+				allowZeroSubverions: false
+			};
+			const version: ISemver = [1, 2, 3];
+
+			it('should require all parts', () => {
+
+				expect(
+					getRequiredSemverPartsCount(version, options)
+				).toBe(
+					3
+				);
+			});
+
+			it('should require major and minor parts', () => {
+
+				const ignorePatchOptions = {
+					...options,
+					ignorePatch: true
+				};
+
+				expect(
+					getRequiredSemverPartsCount(version, ignorePatchOptions)
+				).toBe(
+					2
+				);
+			});
+
+			it('should require major part', () => {
+
+				const ignoreMinorOptions = {
+					...options,
+					ignoreMinor: true
+				};
+
+				expect(
+					getRequiredSemverPartsCount(version, ignoreMinorOptions)
+				).toBe(
+					1
+				);
+			});
+		});
+
+		describe('allowZeroSubverions: true', () => {
+
+			const options = {
+				ignoreMinor:         false,
+				ignorePatch:         false,
+				allowZeroSubverions: true
+			};
+			const version: ISemver = [1, 2, 3];
+			const zeroVersion: ISemver = [1, 1, 0];
+			const zerosVersion: ISemver = [1, 0, 0];
+
+			it('should require all parts', () => {
+
+				expect(
+					getRequiredSemverPartsCount(version, options)
+				).toBe(
+					3
+				);
+
+				expect(
+					getRequiredSemverPartsCount(zeroVersion, options)
+				).toBe(
+					2
+				);
+
+				expect(
+					getRequiredSemverPartsCount(zerosVersion, options)
+				).toBe(
+					1
+				);
+			});
+
+			it('should require major and minor parts', () => {
+
+				const ignorePatchOptions = {
+					...options,
+					ignorePatch: true
+				};
+
+				expect(
+					getRequiredSemverPartsCount(version, ignorePatchOptions)
+				).toBe(
+					2
+				);
+
+				expect(
+					getRequiredSemverPartsCount(zeroVersion, ignorePatchOptions)
+				).toBe(
+					2
+				);
+
+				expect(
+					getRequiredSemverPartsCount(zerosVersion, ignorePatchOptions)
+				).toBe(
+					1
+				);
+			});
+
+			it('should require major part', () => {
+
+				const ignoreMinorOptions = {
+					...options,
+					ignoreMinor: true
+				};
+
+				expect(
+					getRequiredSemverPartsCount(version, ignoreMinorOptions)
+				).toBe(
+					1
+				);
+
+				expect(
+					getRequiredSemverPartsCount(zeroVersion, ignoreMinorOptions)
+				).toBe(
+					1
+				);
+
+				expect(
+					getRequiredSemverPartsCount(zerosVersion, ignoreMinorOptions)
+				).toBe(
+					1
+				);
+			});
+		});
+	});
+
+	describe('rangedSemverToRegExp', () => {
+
+		const options = {
+			ignoreMinor:         false,
+			ignorePatch:         false,
+			allowHigherVersions: false
+		};
+
+		it('should return only numbers', () => {
+
+			expect(
+				rangedSemverToRegExp(
+					[11, 12, 0],
+					options
+				)
+			).toEqual(
+				['11', '12', '0']
+			);
+		});
+
+		it('should return only numbers patterns', () => {
+
+			expect(
+				rangedSemverToRegExp(
+					['all', 0, 0] as any,
+					options
+				)
+			).toEqual(
+				['\\d+', '\\d+', '\\d+']
+			);
+		});
+
+		it('should return number pattern at patch', () => {
+
+			const ignorePatchOptions = {
+				...options,
+				ignorePatch: true
+			};
+
+			expect(
+				rangedSemverToRegExp(
+					[11, 12, 0],
+					ignorePatchOptions
+				)
+			).toEqual(
+				['11', '12', '\\d+']
+			);
+		});
+
+		it('should return number patterns at minor and patch', () => {
+
+			const ignoreMinorOptions = {
+				...options,
+				ignoreMinor: true
+			};
+
+			expect(
+				rangedSemverToRegExp(
+					[11, 12, 0],
+					ignoreMinorOptions
+				)
+			).toEqual(
+				['11', '\\d+', '\\d+']
+			);
+		});
+
+		it('should return ranged major', () => {
+
+			expect(
+				rangedSemverToRegExp(
+					[[11, 13], 12, 0],
+					options
+				)
+			).toEqual(
+				['1[1-3]', '12', '0']
+			);
+		});
+
+		it('should return ranged major ray', () => {
+
+			const allowHigherOptions = {
+				...options,
+				allowHigherVersions: true
+			};
+
+			expect(
+				rangedSemverToRegExp(
+					[[11, 13], 12, 0],
+					allowHigherOptions
+				)
+			).toEqual([
+				'(1[1-9]|[2-9]\\d|\\d{3,})',
+				'(1[2-9]|[2-9]\\d|\\d{3,})',
+				'(\\d|\\d{2,})'
+			]);
+		});
+	});
+});
--- src/semver/semver.ts
+++ src/semver/semver.ts
@@ -9,10 +9,20 @@ import {
 	rangeToRegExp
 } from '../regexp';
 
+/**
+ * Check target if is 'all'.
+ * @param  version - Target to check.
+ * @return Is 'all' or not.
+ */
 export function isAllVersion(version: any): version is 'all' {
 	return version === 'all';
 }
 
+/**
+ * Get semver from string or array.
+ * @param  version - Target to convert.
+ * @return Array with semver parts.
+ */
 export function semverify(version: ISemverLike): ISemver {
 
 	const split = Array.isArray(version)
@@ -37,6 +47,13 @@ export function semverify(version: ISemverLike): ISemver {
 	}) as ISemver;
 }
 
+/**
+ * Compare semvers.
+ * @param  a - Semver to compare.
+ * @param  b - Semver to compare with.
+ * @param  options - Compare options.
+ * @return Equals or not.
+ */
 export function compareSemvers(
 	[
 		major,
@@ -59,11 +76,15 @@ export function compareSemvers(
 		return true;
 	}
 
+	const compareMinor = !ignoreMinor;
+	// const comparePatch = ignoreMinor ? false : !ignorePatch;
+	const comparePatch = compareMinor && !ignorePatch;
+
 	if (allowHigherVersions) {
 
 		if (
-			!ignorePatch && patch < patchBase
-			|| !ignoreMinor && minor < minorBase
+			comparePatch && patch < patchBase
+			|| compareMinor && minor < minorBase
 		) {
 			return false;
 		}
@@ -72,8 +93,8 @@ export function compareSemvers(
 	}
 
 	if (
-		!ignorePatch && patch !== patchBase
-		|| !ignoreMinor && minor !== minorBase
+		comparePatch && patch !== patchBase
+		|| compareMinor && minor !== minorBase
 	) {
 		return false;
 	}
@@ -81,30 +102,12 @@ export function compareSemvers(
 	return major === majorBase;
 }
 
-export function rangedSemverToRegExp(rangedVersion: IRangedSemver, {
-	ignoreMinor,
-	ignorePatch,
-	allowHigherVersions
-}: ISemverCompareOptions) {
-
-	const ignoreIndex = isAllVersion(rangedVersion[0])
-		? 0
-		: ignoreMinor
-			? 1
-			: ignorePatch
-				? 2
-				: Infinity;
-	const numberPatterns: string[] = rangedVersion.map((range, i) =>
-		i >= ignoreIndex
-			? NUMBER_PATTERN
-			: Array.isArray(range)
-				? rangeToRegExp(range[0], allowHigherVersions ? Infinity : range[1])
-				: range.toString()
-	);
-
-	return numberPatterns;
-}
-
+/**
+ * Get required semver parts count.
+ * @param  version - Semver parts or ranges.
+ * @param  options - Semver compare options.
+ * @return Required semver parts count.
+ */
 export function getRequiredSemverPartsCount(version: ISemver|IRangedSemver, {
 	ignoreMinor,
 	ignorePatch,
@@ -130,4 +133,50 @@ export function getRequiredSemverPartsCount(version: ISemver|IRangedSemver, {
 	}
 
 	return shouldRepeatCount;
+}
+
+/**
+ * Ranged semver to regexp patterns.
+ * @param  rangedVersion - Ranged semver.
+ * @param  options - Semver compare options.
+ * @return Array of regexp pattern strings.
+ */
+export function rangedSemverToRegExp(rangedVersion: IRangedSemver, {
+	ignoreMinor,
+	ignorePatch,
+	allowHigherVersions
+}: ISemverCompareOptions) {
+
+	const ignoreIndex = isAllVersion(rangedVersion[0])
+		? 0
+		: ignoreMinor
+			? 1
+			: ignorePatch
+				? 2
+				: Infinity;
+	const numberPatterns: string[] = rangedVersion.map((range, i) => {
+
+		if (i >= ignoreIndex) {
+			return NUMBER_PATTERN;
+		}
+
+		const isRange = Array.isArray(range);
+
+		if (!isRange && !allowHigherVersions) {
+			return range.toString();
+		}
+
+		if (isRange) {
+			return rangeToRegExp(
+				range[0],
+				allowHigherVersions
+					? Infinity
+					: range[1]
+			);
+		}
+
+		return rangeToRegExp(range as number);
+	});
+
+	return numberPatterns;
 }
